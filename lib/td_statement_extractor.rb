2a6a2e2b1c4ed1aa9ee51a19189bc3aea88f8d93
Aaron Gough <aaron.gough@gmail.com>
2020-04-14 17:22:03 -0400

    First commit.

---
+++ lib/td_statement_extractor.rb
@@ -0,0 +1,6 @@
+require "td_statement_extractor/version"
+
+module TdStatementExtractor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/td_statement_extractor/version.rb
@@ -0,0 +1,3 @@
+module TdStatementExtractor
+  VERSION = "0.1.0"
+end
